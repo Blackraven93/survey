0203111ed4a611a4841b620f3afd2075fd54f4bf
Blackraven93 <reblackraven@gmail.com>
2023-01-16 21:54:11 +0900

    feat(local): add compositionLocal state

--- app/src/main/java/com/jetpack/composedemo/Const.kt
+++
@@ -1,6 +0,0 @@
-package com.jetpack.composedemo
-
-const val CALENDAR = "CALENDAR"
-const val TIMELINE = "TIMELINE"
-const val ANALYSIS = "ANALYSIS"
-const val SETTINGS = "SETTINGS"
--- app/src/main/java/com/jetpack/composedemo/MainActivity.kt
+++ app/src/main/java/com/jetpack/composedemo/MainActivity.kt
@@ -1,27 +1,17 @@
 package com.jetpack.composedemo
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.navigation.NavHost
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
 import com.jetpack.composedemo.ui.theme.ComposeDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,171 +19,82 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeDemoTheme() {
-                MainScreenView()
+
             }
         }
     }
 }
 
+val LocalColor = staticCompositionLocalOf { Color(0xFFffdbcf) }
+
 @Composable
-fun MainScreenView() {
-    val navController = rememberNavController()
-    Scaffold(
-        bottomBar = { BottomNavigation(navController = navController) }
-    ) {
-        Box(Modifier.padding(it)){
-            NavigationGraph(navController = navController)
+fun Composable1() {
+    var color = if (isSystemInDarkTheme()) {
+        Color(0xFFa08d87)
+    } else {
+        Color(0xFFffdbcf)
+    }
+
+    Column {
+        Composable2()
+
+        CompositionLocalProvider(LocalColor provides color) {
+            Composable3()
         }
+        Composable3()
     }
 }
 
 @Composable
-fun BottomNavigation(navController: NavHostController) {
-    val items = listOf<BottomNavItem>(
-        BottomNavItem.Calendar,
-        BottomNavItem.Timeline,
-        BottomNavItem.Analysis,
-        BottomNavItem.Settings
-    )
-
-    androidx.compose.material.BottomNavigation(
-        backgroundColor = Color.White,
-        contentColor = Color(0xFF3F414E)
-    ) {
-        val navBackStackEntry by navController.currentBackStackEntryAsState()
-        val currentRoute = navBackStackEntry?.destination?.route
-
-        items.forEach { item ->
-            BottomNavigationItem(
-                icon = {
-                    Icon(
-                        painter = painterResource(id = item.icon),
-                        contentDescription = stringResource(id = item.title),
-                        modifier = Modifier
-                            .width(26.dp)
-                            .height(26.dp)
-                    )
-                },
-                label = { Text(stringResource(id = item.title), fontSize = 9.sp) },
-                selectedContentColor = MaterialTheme.colors.primary,
-                unselectedContentColor = Color.Gray,
-                selected = currentRoute == item.screenRoute,
-                alwaysShowLabel = false,
-                onClick = {
-                    navController.navigate(item.screenRoute) {
-                        navController.graph.startDestinationRoute?.let {
-                            popUpTo(it) { saveState = true }
-                        }
-                        launchSingleTop = true
-                        restoreState = true
-                    }
-                }
-            )
-        }
-    }
-}
-
-sealed class BottomNavItem(
-    val title: Int, val icon: Int, val screenRoute: String
-) {
-    object Calendar : BottomNavItem(R.string.text_calendar, R.drawable.raven_img, CALENDAR)
-    object Timeline : BottomNavItem(R.string.text_timeline, R.drawable.raven_img, TIMELINE)
-    object Analysis : BottomNavItem(R.string.text_analysis, R.drawable.raven_img, ANALYSIS)
-    object Settings : BottomNavItem(R.string.text_settings, R.drawable.raven_img, SETTINGS)
+fun Composable2() {
+    Composable4()
 }
 
 @Composable
-fun NavigationGraph(navController: NavHostController) {
-    NavHost(navController = navController, startDestination = BottomNavItem.Calendar.screenRoute) {
-        composable(BottomNavItem.Calendar.screenRoute) {
-            CalendarScreen()
-        }
-        composable(BottomNavItem.Timeline.screenRoute) {
-            TimelineScreen()
-        }
-        composable(BottomNavItem.Analysis.screenRoute) {
-            AnalysisScreen()
-        }
-        composable(BottomNavItem.Settings.screenRoute) {
-            SettingsScreen()
-        }
-    }
-}
-
-
-@Composable
-fun CalendarScreen() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colors.primary)
-    ) {
-        Text(
-            text = stringResource(id = R.string.text_calendar),
-            style = MaterialTheme.typography.h1,
-            textAlign = TextAlign.Center,
-            color = Color.White,
-            modifier = Modifier.align(Alignment.Center)
-        )
+fun Composable3() {
+    Text("Composable 3", modifier = Modifier.background(LocalColor.current))
+    CompositionLocalProvider(LocalColor provides Color.Red) {
+        Composable5()
     }
 }
 
 @Composable
-fun TimelineScreen() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colors.primaryVariant)
-    ) {
-        Text(
-            text = stringResource(id = R.string.text_timeline),
-            style = MaterialTheme.typography.h1,
-            textAlign = TextAlign.Center,
-            color = Color.White,
-            modifier = Modifier.align(Alignment.Center)
-        )
+fun Composable4() {
+    Composable6()
+}
+
+@Composable
+fun Composable5() {
+    Text("Composable 5", modifier = Modifier.background(LocalColor.current))
+
+    CompositionLocalProvider(LocalColor provides Color.Green) {
+        Composable7()
+    }
+
+    CompositionLocalProvider(LocalColor provides Color.Yellow) {
+        Composable8()
     }
 }
 
 @Composable
-fun AnalysisScreen() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colors.secondary)
-    ) {
-        Text(
-            text = stringResource(id = R.string.text_analysis),
-            style = MaterialTheme.typography.h1,
-            textAlign = TextAlign.Center,
-            color = Color.White,
-            modifier = Modifier.align(Alignment.Center)
-        )
-    }
+fun Composable6() {
+    Text("Composable 6", modifier = Modifier.background(LocalColor.current))
 }
 
 @Composable
-fun SettingsScreen() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colors.secondaryVariant)
-    ) {
-        Text(
-            text = stringResource(id = R.string.text_settings),
-            style = MaterialTheme.typography.h1,
-            textAlign = TextAlign.Center,
-            color = Color.White,
-            modifier = Modifier.align(Alignment.Center)
-        )
-    }
+fun Composable7() {
+    Text("Composable 7", modifier = Modifier.background(LocalColor.current))
 }
 
+@Composable
+fun Composable8() {
+    Text("Composable 8", modifier = Modifier.background(LocalColor.current))
+}
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
 @Composable
 fun DefaultPreview() {
     ComposeDemoTheme {
-        MainScreenView()
+        Composable1()
     }
 }
